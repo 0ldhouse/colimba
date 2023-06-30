8d959261bce2a8ee4388892aed44d4752421bf86
0ldhouse <136765116+0ldhouse@users.noreply.github.com>
2023-06-30 13:49:21 -0300

    Add files via upload

---
+++ 1/13/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LENGHT 20
+int main()
+int c,(lenths [MAX_LENGHT])}(=0);
+while ((c=getchar ())!=EOF){/n
+    if (C==''||C=='\t'||C=='\n'){
+        lenghts[lenght - 1]++;
+    }
+    length = 0;
+} else {
+length++;
+   }
+
+}
+printf("word lengths historigram:\n");
+for (int i]=0; <MAX_LENGTH;i++){
+    printf("%2d:",i+1);
+    for(int j=0;j<lengths([i];j++){
+        putchar('*');
+    }
+    putchar('\n');
+}
+return 0
