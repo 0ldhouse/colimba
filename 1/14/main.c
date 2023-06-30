7e2ba5ba0ee4c4109c24fbc73af67f92b0d060b5
0ldhouse <136765116+0ldhouse@users.noreply.github.com>
2023-06-30 13:50:38 -0300

    Add files via upload

---
+++ 1/14/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define MAX_CHAR 128
+int main(){
+int c;
+int frequencies[MAX_CHAR]={0};
+while ((c=getchar)!=EOF)
+    (frequencies[c])++;
+}
+}
+printf ("character frequencies histogram")\n");
+for (int i=0;i<MAX_CHAR;i++){
+    printf("3d"(´%c´):",i,i);
+    for(int j=0;j<frequencies[i];j++){
+        putchar('\n');
+    }
+}
+return 0;
+}
