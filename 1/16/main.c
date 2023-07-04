acb27da86c318843b45d258142f006050a901b3c
0ldhouse <136765116+0ldhouse@users.noreply.github.com>
2023-07-04 08:29:52 -0300

    Update main.c

--- 1/16/main.c
+++ 1/16/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#define MAX_LINE 1000
-#define MIN_LENGHT 80
+#define MAXLINE 1000
+#define MINLENGHT 80
 int main(){
-char line [MAX_LINE];
+char line [MAXLINE];
 int length=0;
-while((line,MAX_LINE,stdin)!='\0'{
+while((line,[MAXLINE]!='\0'{
     lenght++;
 }
 if (length>=MIN_LENGTH){
